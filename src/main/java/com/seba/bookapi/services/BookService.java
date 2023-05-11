1ed0941dfc53595c2d6e4a21c6f94d209c9dd74f
Sebaplz <sebastian.h.neira@outlook.com>
2023-05-10 20:19:52 -0400

    Initial Commit

---
+++ src/main/java/com/seba/bookapi/services/BookService.java
@@ -0,0 +1,71 @@
+package com.seba.bookapi.services;
+
+import com.seba.bookapi.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+import com.seba.bookapi.models.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+    //Agregando el book al repositorio como una dependencia
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    //Devolviendo todos los libros.
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+
+    //Creando un libro.
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+
+    //Obteniendo la información de un libro
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+
+    public Book updateBook(Long id, String title, String desc, String lang, int pages){
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            Book bookToUpdate = optionalBook.get();
+            if (title != null){
+                bookToUpdate.setTitle(title);
+            }
+            if(desc != null){
+                bookToUpdate.setDescription(desc);
+            }
+            if (lang != null){
+                bookToUpdate.setLanguage(lang);
+            }
+            bookToUpdate.setNumberOfPages(pages);
+            bookRepository.save(bookToUpdate);
+            return bookToUpdate;
+        } else {
+            return null;
+        }
+    }
+
+
+    public Book deleteBook(Long id){
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()){
+            bookRepository.deleteById(id);
+            return optionalBook.get();
+        }else{
+            return null;
+        }
+    }
+}
+
